e6d2a9e3007788589889740402c135047307ec12
WizMager <wizmager@gmail.com>
2022-08-02 18:56:08 +0300

    feat: add button and label in playFab scene, add change color and text when handle login(error or success connect) feat: add button in photon scene, add handle press on this button(connect or disconnect and change text and color of text in button)

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using Button = UnityEngine.UI.Button;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Button _loginButton; 
+    private TextMeshProUGUI _loginButtonText;
+    private bool _isConnected;
+    
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        _loginButtonText = _loginButton.GetComponentInChildren<TextMeshProUGUI>();
+        _loginButtonText.text = "Login";
+        _loginButton.onClick.AddListener(LoginButton);
+        //Connect();
+    }
+
+    private void LoginButton()
+    {
+        if (_isConnected)
+        {
+           Disconnect(); 
+        }
+        else
+        {
+            Connect();
+        }
+    }
+    
+    private void Connect()
+    {
+        _loginButtonText.text = "Connect...";
+        if (PhotonNetwork.IsConnected)
+            return;
+
+        PhotonNetwork.ConnectUsingSettings();
+        //PhotonNetwork.GameVersion = Application.version;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        _isConnected = true;
+        _loginButtonText.text = "Connected";
+        _loginButtonText.color = Color.green;
+        Debug.Log("Connected to Master Server.");
+    }
+
+    private void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        _isConnected = false;
+        _loginButtonText.text = "Disconnected";
+        _loginButtonText.color = Color.red;
+        Debug.Log("Disconnected.");
+    }
+
+    private void OnDestroy()
+    {
+        _loginButton.onClick.RemoveListener(LoginButton);
+    }
+}
---
+++ Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,39 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _loginLabel;
+    private void Start()
+    {
+        _loginLabel.text = "Login...";
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            PlayFabSettings.staticSettings.TitleId = "28CE7";
+        }
+
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = "CustomId",
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginError);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        _loginLabel.text = "Login is success!";
+        _loginLabel.color = Color.green;
+       Debug.Log("Login is success!"); 
+    }
+
+    private void OnLoginError(PlayFabError error)
+    {
+        _loginLabel.text = "Login ERROR!";
+        _loginLabel.color = Color.red;
+        var errorMessage = error.GenerateErrorReport();
+        Debug.Log(errorMessage);
+    }
+}
